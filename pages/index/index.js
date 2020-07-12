769706bf00c6d189c7007a908a238eac24b941b6
lin <1845948316@qq.com>
2020-07-12 23:49:25 +0800

    首页样式完善

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,65 +1,57 @@
+// 导入网络请求
+import { request } from "../../request/index.js";
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+    // 轮播图数据
+    swiperList: [],
+    // 导航 数组
+    catesList: [],
+    // 楼层数据
+    floorList: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    this.getSwiperList()
+    this.getCateList()
+    this.getFloorList()
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
+  // 获取轮播图数据
+  getSwiperList() {
+    request({ url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata' })
+      .then(result => {
+        this.setData({
+          swiperList: result.data.message
+        })
+      })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
+  // 获取分类导航数据
+  getCateList() {
+    request({ url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/catitems' })
+      .then(result => {
+        this.setData({
+          catesList: result.data.message
+        })
+      })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+  // 获取楼层数据
+  // 获取分类导航数据
+  getFloorList() {
+    request({ url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/floordata' })
+      .then(result => {
+        this.setData({
+          floorList: result.data.message
+        })
+      })
   }
 })
