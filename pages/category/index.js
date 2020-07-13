8ad074a39e91b5d07081ba817e071e3a9ce4cf78
lin <1845948316@qq.com>
2020-07-13 23:02:54 +0800

    分类页面基本样式

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,45 @@
 // pages/category/index.js
+import { request } from '../../request/index.js'
 Page({
-
   /**
-   * 页面的初始数据
-   */
+    * 页面的初始数据
+    */
   data: {
-
+    // 左侧菜单数据
+    leftMenuList: [],
+    // 右侧商品数据
+    rightContent: [],
+    // 左侧被点击的菜单
+    currentIndex: 0
   },
+  // 接口返回的数据
+  Cates: [],
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.getCates()
 
   },
+  // 获取分类数据
+  getCates() {
+    request({
+      url: "https://api-hmugo-web.itheima.net/api/public/v1/categories"
+    })
+      .then(res => {
+        console.log(res);
+        this.Cates = res.data.message
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+        // 构造左侧菜单数据
+        let leftMenuList = this.Cates.map(n => n.cat_name)
+        // 构造右侧商品数据
+        let rightContent = this.Cates[0].children
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+        this.setData({
+          leftMenuList,
+          rightContent
+        })
+      })
   }
 })
