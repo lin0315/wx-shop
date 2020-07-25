50a9738b20aa68e8d18665fe88e7ad16479c5993
lin <1845948316@qq.com>
2020-07-25 23:10:22 +0800

    意见反馈页面样式

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -5,62 +5,29 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    tabs: [
+      {
+        id: 0,
+        value: '体验问题',
+        isActive: true
+      },
+      {
+        id: 1,
+        value: '商品、商品投诉',
+        isActive: false
+      }
+    ],
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handletabsItenChange(e) {
+    // 获取被点击的索引
+    const { index } = e.detail
+    // 获取原数据 进行遍历
+    let { tabs } = this.data
+    tabs.forEach((n, i) => i === index ? n.isActive = true : n.isActive = false);
+    // 赋值
+    this.setData({
+      tabs
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
--- pages/search/index.js
+++ pages/search/index.js
@@ -1,66 +1,55 @@
 // pages/search/index.js
+import { request } from '../../request/index.js'
+import regeneratorRuntime from '../../lib/runtime/runtime'
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    goods: [],
+    // 按钮是否隐藏
+    isFocus: false,
+    // 输入框的值
+    inpValue: ""
   },
+  TimeId: -1,
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  // 监听输入框事件
+  handleInput(e) {
+    const { value } = e.detail
+    // 判断是否为空格
+    if (!value.trim()) {
+      // 不合理
+      this.setData({
+        goods: [],
+        isFocus: false
+      })
+      return
+    }
+    // 发送请求
+    this.setData({
+      isFocus: true
+    })
+    clearTimeout(this.TimeId);
+    this.TimeId = setTimeout(() => {
+      this.qsearch(value)
+    }, 800)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 请求数据
+  async qsearch(query) {
+    const res = await request({ url: '/goods/search', data: { query } })
+    console.log(res.goods);
+    this.setData({
+      goods: res.goods
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleCancel() {
+    this.setData({
+      inpValue: "",
+      goods: [],
+      isFocus: false
+    })
   }
 })
